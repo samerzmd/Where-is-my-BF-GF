c1d856ed57af8656e9928c1e068d38ce0963679b
samerzmd <samerzmd@gmail.com>
2015-12-30 19:57:31 +0200

    resturture

---
+++ WhereismyBFGF/app/src/main/java/com/dragoneel/samer/whereismybfgf/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dragoneel.samer.whereismybfgf;
+//create new branch
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
